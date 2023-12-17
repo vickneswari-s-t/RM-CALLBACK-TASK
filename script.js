2e6c94f18ac7e7221316d5ffc5c89b841253948e
vickneswari-s-t <vickneswari.s.t@gmail.com>
2023-12-17 15:49:22 +0530

    update call task

---
+++ script.js
@@ -0,0 +1,48 @@
+
+setTimeout(()=>{
+  document.getElementById("count down").innerText = "10";
+  
+
+setTimeout(()=>{
+  document.getElementById("count down").innerText="9";
+
+  setTimeout(()=>{
+    document.getElementById("count down").innerText= "8";
+
+    setTimeout(()=>{
+      document.getElementById("count down").innerText="7";
+
+      setTimeout(()=>{
+        document.getElementById("count down").innerText="6";
+
+        setTimeout(()=>{
+          document.getElementById("count down").innerText="5";
+
+          setTimeout(()=>{
+            document.getElementById("count down").innerText="4";
+            setTimeout(()=>{
+              document.getElementById("count down").innerText="3";
+
+              setTimeout(()=>{
+                document.getElementById("count down").innerText="2";
+
+                setTimeout(()=>{
+                  document.getElementById("count down").innerText="1";
+
+                  setTimeout(()=>{
+                    document.getElementById("count down").innerText="0";
+                    setTimeout(()=>{
+                      document.getElementById("count down").innerText="HAPPY  INDEPENDENCE  DAY";
+                    },1000);
+                  },1000);
+                },1000);
+              },1000);
+            },1000);
+          },1000);
+        },1000);
+      },1000);
+    },1000);
+  },1000);
+},1000); 
+              
+},1000);
